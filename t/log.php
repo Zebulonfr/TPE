316adb315d63a0a193e514021e43c27bc26355a8
Lord_All <beauvais.alan@gmail.com>
2018-01-06 15:54:56 +0100

    Add log page for connect and in the future add new definition

---
+++ t/log.php
@@ -0,0 +1,32 @@
+<?php
+  session_start();
+  if (isset($_POST['formDone'])) {
+    if (isset($_POST['identif']) AND isset($_POST['pwd'])) {
+      $iden = $_POST['identif'];
+      $pwd = $_POST['pwd'];
+
+      if ($iden == "admin") {
+        if ($pwd == "adminadmin") {
+          $_SESSION['iden'] = $iden;
+          header('Location: addDef.php');
+        }
+      }
+    }
+  }
+ ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Log</title>
+  </head>
+  <body>
+    <div align="center" style="padding-top: 100px;">
+      <form method="post">
+        <input type="text" name="identif"> <br><br>
+        <input type="password" name="pwd"> <br><br>
+        <input type="submit" name="formDone">
+      </form>
+    </div>
+  </body>
+</html>
